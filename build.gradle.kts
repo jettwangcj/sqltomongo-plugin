bc770db2fc91200558b8b7d41403d3a2cfdc4fb1
wangchangjiu <wangchangjiu@rr.tv>
2023-05-04 11:01:41 +0800

    修改配置

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,13 +22,13 @@ dependencies {
     implementation(kotlin("stdlib"))
 }
 
-// Configure Gradle IntelliJ Plugin
+// Configure Gradle IntelliJ Pluginunresolved
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
     version.set("2022.1.4")
     type.set("IC") // Target IDE Platform
 
-    plugins.set(listOf(/* Plugin Dependencies */))
+    //plugins.set(listOf(/* Plugin Dependencies */))
 }
 
 
